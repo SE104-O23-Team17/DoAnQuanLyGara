1747a9298b6ff499b4e073dea974282326648b93
beobruh <huytrieutan@gmail.com>
2024-05-17 23:00:06 +0700

    checkpoint 1

---
+++ QuanLyGara/Models/DanhSachThang/DanhSachThangModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyGara.Models.DanhSachThang
+{
+
+    public class DanhSachThangModel
+    {
+        public DanhSachThangModel()
+        {
+            maThang = 0;
+            tenThang = "";
+        }
+        public int maThang { get; set; }
+
+        public string tenThang { get; set; }
+    }
+}
