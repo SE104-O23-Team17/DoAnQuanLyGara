3533063a8be19a4fbf6f42e1ae98e37b2842af5d
quandohong109 <quandohong109@gmail.com>
2024-05-27 07:56:06 +0700

    done

--- QuanLyGara/ViewModels/Pages/UserViewModel.cs
+++ QuanLyGara/ViewModels/Pages/UserViewModel.cs
@@ -4,15 +4,143 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using QuanLyGara.Models;
+using QuanLyGara.Services;
+using QuanLyGara.Views.Windows;
 
 namespace QuanLyGara.ViewModels.Pages
 {
     public class UserViewModel : ViewModelBase
     {
+        private IDialogService dialogService;
         private GaraModel gara;
         public GaraModel getGara
+        {
+            get { return gara; }
+        }
+
+        private string _oldPassword;
+        private string _newPassword;
+        private string _confirmPass;
+        public string OldPassword
+        {
+            get { return _oldPassword; }
+            set
+            {
+                _oldPassword = value;
+                OnPropertyChanged("OldPassword");
+            }
+        }
+        public string NewPassword
+        {
+            get { return _newPassword; }
+            set
+            {
+                _newPassword = value;
+                OnPropertyChanged("NewPassword");
+                UpdatePasswordConfirmationError();
+            }
+        }
+        public string ConfirmPass
+        {
+            get { return _confirmPass; }
+            set
+            {
+                _confirmPass = value;
+                OnPropertyChanged("ConfirmPass");
+                UpdatePasswordConfirmationError();
+            }
+        }
+        public ICommand LogOut { get; }
+        public ICommand ChangePasswordCommand { get; }
         public UserViewModel()
         {
+            dialogService = new DialogService();
+            gara = Global.Instance.garaHienTai; 
+            PasswordConfirmationError = "";
+
+            LogOut = new ViewModelCommand(ExecuteLogOut);
+            ChangePasswordCommand = new ViewModelCommand(ExecuteChangePasswordCommand);
+        }
+
+        private void ExecuteLogOut(object obj)
+        {
+            dialogService.ShowYesNoDialog(
+                "Đăng xuất",
+                "Bạn có muốn đăng xuất không?", 
+                () => {
+                    Register loginView = new Register();
+                    loginView.Show();
+
+                    var mainWindow = obj as MainWindow;
+                    mainWindow.Close();
+                },
+                () => { }
+                );
+        }
+
+        private void ExecuteChangePasswordCommand(object obj)
+        {
+            if (string.IsNullOrWhiteSpace(OldPassword) || string.IsNullOrWhiteSpace(NewPassword) || string.IsNullOrWhiteSpace(ConfirmPass))
+            {
+                dialogService.ShowInfoDialog(
+                    "Thông báo",
+                    "Vui lòng điền đầy đủ thông tin.",
+                    () => { }
+                );
+                return;
+            }
+
+            if (!OldPassword.Equals(gara.MatKhau))
+            {
+                dialogService.ShowInfoDialog(
+                    "Thông báo",
+                    "Mật khẩu cũ không chính xác.",
+                    () => { }
+                );
+                return;
+            }
+
+            if (!NewPassword.Equals(ConfirmPass))
+            {
+                dialogService.ShowInfoDialog(
+                    "Thông báo",
+                    "Mật khẩu mới không khớp.",
+                    () => { }
+                );
+                return;
+            }
+            
+            gara.MatKhau = NewPassword;
+            Global.Instance.garaHienTai = gara;
+            Global.Instance.danhSachGara[Global.Instance.danhSachGara.FindIndex(g => g.TaiKhoan == gara.TaiKhoan)] = gara;
+
+            dialogService.ShowInfoDialog(
+                "Thông báo",
+                "Đổi mật khẩu thành công! Vui lòng đăng nhập lại.",
+                () => {
+                    Register loginView = new Register();
+                    loginView.Show();
+
+                    var mainWindow = obj as MainWindow;
+                    mainWindow.Close();
+                }
+            );
+        }
+
+        public string PasswordConfirmationError { get; private set; }
+        private void UpdatePasswordConfirmationError()
+        {
+            if (_confirmPass != null && _confirmPass.Length > 0)
+            {
+                PasswordConfirmationError = _newPassword == _confirmPass ? "" : "Mật khẩu không khớp!";
+            }
+            else
+            {
+                PasswordConfirmationError = "";
+            }
+            OnPropertyChanged("PasswordConfirmationError");
         }
     }
 }
