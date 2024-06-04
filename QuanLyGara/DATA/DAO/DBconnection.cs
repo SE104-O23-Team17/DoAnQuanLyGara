4f979b590890cbbcc60e484eaea74e1d355ade3c
beobruh <huytrieutan@gmail.com>
2024-06-04 10:51:32 +0700

    .

--- QuanLyGara/DATA/DAO/DBconnection.cs
+++ QuanLyGara/DATA/DAO/DBconnection.cs
@@ -10,8 +10,9 @@ namespace QuanLyGara
 {
     public class DBconnection
     {
-        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-6FEF0FT;Initial Catalog=QUANLYGARAOTO;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
 
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-6FEF0FT;Initial Catalog=QUANLYGARAOTO;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        
         // get the connection
         public SqlConnection getConnection
         {
