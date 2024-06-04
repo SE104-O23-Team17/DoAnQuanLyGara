2b9f04af67125bab765c9dc66634ed0af8a86771
quandohong109 <quandohong109@gmail.com>
2024-06-05 04:11:49 +0700

    done report

--- QuanLyGara/ViewModels/Pages/StatisticViewModel.cs
+++ QuanLyGara/ViewModels/Pages/StatisticViewModel.cs
@@ -31,6 +31,9 @@ namespace QuanLyGara.ViewModels.Pages
             }
         }
 
+        public int TonThang { get; set; }
+        public int TonNam { get; set; }
+
         private DanhSachThangModel _selectedThang;
         public DanhSachThangModel SelectedThang
         {
@@ -54,7 +57,8 @@ namespace QuanLyGara.ViewModels.Pages
         }
 
         public ICommand SwitchCommand { get; set; }
-        public ICommand TruyXuatCommand { get; }
+        public ICommand TruyXuatThangCommand { get; }
+        public ICommand TruyXuatTonCommand { get; set; }
 
         public StatisticViewModel()
         {
@@ -62,13 +66,17 @@ namespace QuanLyGara.ViewModels.Pages
             isSwitching = false;
 
             danhSachThang = Global.Instance.danhSachThang;
-            _selectedThang = DanhSachThang.FirstOrDefault(thang => thang.maThang == DateTime.Now.Month);
+            _selectedThang = DanhSachThang.FirstOrDefault(thang => thang.maThang == DateTime.Now.Month - 1);
+
+            TonNam = 0;
+            TonThang = 0;
 
             danhSachNam = GenerateYearList();
             selectedNam = DateTime.Now.Year;
 
             SwitchCommand = new ViewModelCommand(ExecuteSwitchCommand);
-            TruyXuatCommand = new ViewModelCommand(ExecuteTruyXuatCommand);
+            TruyXuatThangCommand = new ViewModelCommand(ExecuteTruyXuatThangCommand);
+            TruyXuatTonCommand = new ViewModelCommand(ExecuteTruyXuatTonCommand);
 
             baoCaoTon = new List<BaoCaoTonModel>();
 
@@ -132,20 +140,35 @@ namespace QuanLyGara.ViewModels.Pages
             }
         }
 
-        private void ExecuteTruyXuatCommand(object obj)
+        private void ExecuteTruyXuatThangCommand(object obj)
         {
-            BaoCaoDoanhSoModel tempBaoCao = null;
-            if (SelectedThang != null && SelectedNam != 0)
+            if (SelectedThang == null || SelectedNam == 0)
             {
-                tempBaoCao = Global.Instance.danhSachDoanhSo.FirstOrDefault(baoCao => baoCao.thang == SelectedThang.maThang && baoCao.nam == SelectedNam);
+                dialogService.ShowInfoDialog(
+                    "Thông báo",
+                    "Vui lòng chọn tháng và năm cần truy xuất.",
+                    () => { }
+                );
+                return;
             }
             
-            baoCaoDoanhSo = tempBaoCao;
-            
+            if (SelectedThang.maThang >= DateTime.Now.Month && SelectedNam >= DateTime.Now.Year)
+            {
+                dialogService.ShowInfoDialog(
+                    "Thông báo",
+                    "Chỉ có thể truy xuất các tháng trước đây.",
+                    () => { }
+                    );
+                return;
+            }
+
+            baoCaoDoanhSo = Global.Instance.danhSachDoanhSo.FirstOrDefault(baoCao => baoCao.thang == SelectedThang.maThang && baoCao.nam == SelectedNam);
+
             if (BaoCaoDoanhSo == null)
             {
+
                 bool hasMatchingPhieuSuaChua = Global.Instance.danhSachPhieuSC.Any(phieuSuaChua =>
-        phieuSuaChua.ngayLap.Month == SelectedThang.maThang && phieuSuaChua.ngayLap.Year == SelectedNam);
+            phieuSuaChua.ngayLap.Month == SelectedThang.maThang && phieuSuaChua.ngayLap.Year == SelectedNam);
 
                 if (!hasMatchingPhieuSuaChua)
                 {
@@ -192,27 +215,165 @@ namespace QuanLyGara.ViewModels.Pages
                 Global.Instance.danhSachDoanhSo.Add(baoCaoDoanhSo);
             }
             OnPropertyChanged(nameof(BaoCaoDoanhSo));
-            
-            List<BaoCaoTonModel> tempBaoCaoTon = new List<BaoCaoTonModel>();
-            
+        }
+
+        private void ExecuteTruyXuatTonCommand(object obj)
+        {
             if (SelectedThang != null && SelectedNam != 0)
             {
-                tempBaoCaoTon = Global.Instance.danhSachTon.Where(baoCao => baoCao.thang == SelectedThang.maThang && baoCao.nam == SelectedNam).ToList();
+                if (SelectedThang.maThang >= DateTime.Now.Month && SelectedNam >= DateTime.Now.Year)
+                {
+                    dialogService.ShowInfoDialog(
+                        "Thông báo",
+                        "Chỉ có thể truy xuất các tháng trước đây.",
+                        () => { }
+                    );
+                    return;
+                }
+
+                var groupedDanhSachTon = Global.Instance.danhSachTon
+                    .GroupBy(baoCao => new { baoCao.thang, baoCao.nam })
+                    .ToDictionary(g => g.Key, g => g.ToList());
+
+                BaoCaoTon = groupedDanhSachTon.FirstOrDefault(baoCao => baoCao.Key.thang == SelectedThang.maThang && baoCao.Key.nam == SelectedNam).Value;
+
+                if (BaoCaoTon != null)
+                {
+                    TonNam = BaoCaoTon.FirstOrDefault().nam;
+                    TonThang = BaoCaoTon.FirstOrDefault().thang;
+                    OnPropertyChanged(nameof(TonNam));
+                    OnPropertyChanged(nameof(TonThang));
+                    return;
+                }
+
+                BaoCaoTon = Global.Instance.danhSachVTPT
+                    .Select(vtpt => new BaoCaoTonModel
+                    {
+                        thang = SelectedThang.maThang,
+                        nam = SelectedNam,
+                        vtpt = vtpt,
+                        tonDau = 0,
+                        tonCuoi = 0,
+                        phatSinh = 0
+                    })
+                    .ToList();
+
+                var latestKey = groupedDanhSachTon.Keys
+                    .OrderByDescending(key => key.nam)
+                    .ThenByDescending(key => key.thang)
+                    .FirstOrDefault();
+
+                if (latestKey != null)
+                {
+                    var latestDateTime = new DateTime(latestKey.nam, latestKey.thang, 1).AddMonths(1).AddDays(-1);
+                    var latestBaoCaoTonList = groupedDanhSachTon[latestKey];
+                    foreach (var latestBaoCaoTon in latestBaoCaoTonList)
+                    {
+                        var matchingBaoCaoTon = BaoCaoTon.FirstOrDefault(bct => bct.vtpt == latestBaoCaoTon.vtpt);
+                        if (matchingBaoCaoTon != null)
+                        {
+                            var sumSoLuongSuaChua = Global.Instance.danhSachPhieuSC
+                                .Where(phieu => phieu.ngayLap > latestDateTime && phieu.ngayLap < new DateTime(SelectedNam, SelectedThang.maThang, 1))
+                                .SelectMany(phieu => phieu.DanhSachCT)
+                                .SelectMany(ct => ct.DanhSachSuDung)
+                                .Where(suDung => suDung.VTPT == latestBaoCaoTon.vtpt)
+                                .Sum(suDung => suDung.SoLuong);
+
+                            var sumSoLuongNhap = Global.Instance.danhSachPhieuNhap
+                                .Where(phieu => phieu.NgayNhap > latestDateTime && phieu.NgayNhap < new DateTime(SelectedNam, SelectedThang.maThang, 1))
+                                .SelectMany(phieu => phieu.DanhSachCT)
+                                .Where(ct => ct.VTPT == latestBaoCaoTon.vtpt)
+                                .Sum(ct => ct.SoLuong);
+
+                            var chenhLech = sumSoLuongNhap - sumSoLuongSuaChua;
+
+                            matchingBaoCaoTon.tonDau = latestBaoCaoTon.tonCuoi + chenhLech;
+                            matchingBaoCaoTon.tonCuoi = latestBaoCaoTon.tonCuoi + chenhLech;
+                            matchingBaoCaoTon.phatSinh = 0;
+                        }
+                    }
+
+                    foreach (var baoCaoTon in latestBaoCaoTonList)
+                    {
+                        var sumSoLuongSuaChua = 0;
+                        var sumSoLuongNhap = 0;
+
+                        sumSoLuongSuaChua = Global.Instance.danhSachPhieuSC
+                            .Where(phieu => phieu.ngayLap > latestDateTime && phieu.ngayLap < new DateTime(SelectedNam, SelectedThang.maThang, 1))
+                            .SelectMany(phieu => phieu.DanhSachCT)
+                            .SelectMany(ct => ct.DanhSachSuDung)
+                            .Where(suDung => suDung.VTPT == baoCaoTon.vtpt)
+                            .Sum(suDung => suDung.SoLuong);
+                       
+                        sumSoLuongNhap = Global.Instance.danhSachPhieuNhap
+                            .Where(phieu => phieu.NgayNhap > latestDateTime && phieu.NgayNhap < new DateTime(SelectedNam, SelectedThang.maThang, 1))
+                            .SelectMany(phieu => phieu.DanhSachCT)
+                            .Where(ct => ct.VTPT == baoCaoTon.vtpt)
+                            .Sum(ct => ct.SoLuong);
+
+                        var chenhLech = sumSoLuongNhap - sumSoLuongSuaChua;
+
+                        var matchingBaoCaoTon = BaoCaoTon.FirstOrDefault(bct => bct.vtpt == baoCaoTon.vtpt);
+                        if (matchingBaoCaoTon != null)
+                        {
+                            matchingBaoCaoTon.tonDau = baoCaoTon.tonCuoi + chenhLech;
+                            matchingBaoCaoTon.tonCuoi = baoCaoTon.tonCuoi + chenhLech;
+                            matchingBaoCaoTon.phatSinh = 0;
+                        }
+                    }
+                }
+
+                var selectedMonthStart = new DateTime(SelectedNam, SelectedThang.maThang, 1);
+                var selectedMonthEnd = selectedMonthStart.AddMonths(1).AddDays(-1);
+
+                foreach (var baoCaoTon in BaoCaoTon)
+                {
+                    var sumSoLuongSuaChua = Global.Instance.danhSachPhieuSC
+                        .Where(phieu => phieu.ngayLap >= selectedMonthStart && phieu.ngayLap <= selectedMonthEnd)
+                        .SelectMany(phieu => phieu.DanhSachCT)
+                        .SelectMany(ct => ct.DanhSachSuDung)
+                        .Where(suDung => suDung.VTPT == baoCaoTon.vtpt)
+                        .Sum(suDung => suDung.SoLuong);
+
+                    var sumSoLuongNhap = Global.Instance.danhSachPhieuNhap
+                        .Where(phieu => phieu.NgayNhap >= selectedMonthStart && phieu.NgayNhap <= selectedMonthEnd)
+                        .SelectMany(phieu => phieu.DanhSachCT)
+                        .Where(ct => ct.VTPT == baoCaoTon.vtpt)
+                        .Sum(ct => ct.SoLuong);
+
+                    baoCaoTon.tonCuoi = baoCaoTon.tonDau + sumSoLuongNhap - sumSoLuongSuaChua;
+                    baoCaoTon.phatSinh = sumSoLuongNhap - sumSoLuongSuaChua;
+                }
+
+                if (BaoCaoTon.Count == 0)
+                {
+                    dialogService.ShowInfoDialog(
+                        "Thông báo",
+                        "Không tìm thấy báo cáo tồn cho tháng và năm được chọn.",
+                        () => { }
+                    );
+                }
+                else
+                {
+                    foreach (var baoCaoTon in BaoCaoTon)
+                    {
+                        Global.Instance.danhSachTon.Add(baoCaoTon);
+                    }
+                    TonNam = BaoCaoTon.FirstOrDefault().nam;
+                    TonThang = BaoCaoTon.FirstOrDefault().thang;
+                    OnPropertyChanged(nameof(TonNam));
+                    OnPropertyChanged(nameof(TonThang));
+                }
             }
-            
-            BaoCaoTon = tempBaoCaoTon;
-            
-            if (BaoCaoTon.Count == 0)
+            else
             {
                 dialogService.ShowInfoDialog(
                     "Thông báo",
-                    "Không tìm thấy báo cáo tồn cho tháng và năm được chọn.",
+                    "Vui lòng chọn tháng và năm cần truy xuất.",
                     () => { }
-                    );
+                );
             }
-            
             OnPropertyChanged(nameof(BaoCaoTon));
         }
-
     }
 }
